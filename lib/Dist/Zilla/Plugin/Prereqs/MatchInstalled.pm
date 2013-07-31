a28012070b9429c52c68d3f01298d053af5ae5c3
Kent Fredric <kentfredric@gmail.com>
2013-08-01 03:18:12 +1200

    Build results of b8ba452 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -85,9 +85,16 @@ sub mvp_multivalue_args { qw(applyto applyto_relation applyto_phase modules) }
 sub mvp_aliases{  return { 'module' => 'modules' }  }
 sub current_version_of {
     my ($self, $package ) = @_;
+    if ( $package eq 'perl' ){
+        # Thats not going to work, Dave.
+        return;
+    }
     require Module::Data;
     my $data = Module::Data->new($package);
     return if not $data;
+    if ( $data->path ){
+        warn $data->path;
+    }
     return $data->version;
 }
 around dump_config => sub {
