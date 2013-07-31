0efdc57dfe394ccbeaed7a20d6e94b3c0496cd94
Kent Fredric <kentfredric@gmail.com>
2013-08-01 03:33:43 +1200

    Build results of 81bdd5e (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -16,123 +16,125 @@ use MooseX::Types::Moose qw( HashRef ArrayRef Str );
 with 'Dist::Zilla::Role::PrereqSource';
 
 
-has applyto_phase => ( 
-    is => ro =>,
-    isa => ArrayRef[Str] =>,
-    lazy => 1,
-    default => sub { [qw(build test runtime configure develop)] }
+has applyto_phase => (
+  is => ro =>,
+  isa => ArrayRef [Str] =>,
+  lazy    => 1,
+  default => sub { [qw(build test runtime configure develop)] }
 );
 
-has applyto_relation => ( 
-    is => ro =>
-    isa => ArrayRef[Str],
-    lazy => 1,
-    default => sub { [qw(requires recommends suggests)] }
+has applyto_relation => (
+  is => ro => isa => ArrayRef [Str],
+  lazy    => 1,
+  default => sub { [qw(requires recommends suggests)] }
 );
 
 has applyto => (
-    is => ro =>,
-    isa => ArrayRef[Str] =>,
-    lazy => 1,
-    builder => _build_applyto =>,
+  is => ro =>,
+  isa => ArrayRef [Str] =>,
+  lazy    => 1,
+  builder => _build_applyto =>,
 );
 has _applyto_list => (
-    is => ro =>,
-    isa => ArrayRef[ArrayRef[Str]],
-    lazy => 1, 
-    builder => _build__applyto_list =>,
+  is => ro =>,
+  isa => ArrayRef [ ArrayRef [Str] ],
+  lazy    => 1,
+  builder => _build__applyto_list =>,
 );
 
 has modules => (
-    is => ro =>,
-    isa => ArrayRef[Str],
-    lazy => 1,
-    default => sub { [] }
+  is => ro =>,
+  isa => ArrayRef [Str],
+  lazy    => 1,
+  default => sub { [] }
 );
-has _modules_hash => ( 
-    is => ro =>,
-    isa => HashRef,
-    lazy => 1,
-    builder => _build__modules_hash =>,
+has _modules_hash => (
+  is      => ro                   =>,
+  isa     => HashRef,
+  lazy    => 1,
+  builder => _build__modules_hash =>,
 );
+
 sub _build_applyto {
-    my $self = shift;
-    my @out;
-    for my $phase (@{ $self->applyto_phase } ) {
-        for my $relation (@{ $self->applyto_relation }){ 
-            push @out, $phase . q[.] . $relation;
-        }
+  my $self = shift;
+  my @out;
+  for my $phase ( @{ $self->applyto_phase } ) {
+    for my $relation ( @{ $self->applyto_relation } ) {
+      push @out, $phase . q[.] . $relation;
     }
-    return \@out;
+  }
+  return \@out;
 }
+
 sub _build__applyto_list {
-    my $self = shift;
-    my @out; 
-    for my $type (@{ $self->applyto }) {
-        if ( $type =~ /^([^.]+)[.]([^.]+)$/ ) {
-            push @out, ["$1", "$2" ];
-            next;
-        }
-        die "<<$type>> does not match << <phase>.<relation> >>";
+  my $self = shift;
+  my @out;
+  for my $type ( @{ $self->applyto } ) {
+    if ( $type =~ /^([^.]+)[.]([^.]+)$/ ) {
+      push @out, [ "$1", "$2" ];
+      next;
     }
-    return \@out;
+    die "<<$type>> does not match << <phase>.<relation> >>";
+  }
+  return \@out;
 }
+
 sub _build__modules_hash {
-    my $self = shift;
-    return { map { $_ , 1 } @{ $self->modules } };
+  my $self = shift;
+  return { map { $_, 1 } @{ $self->modules } };
 }
 sub mvp_multivalue_args { qw(applyto applyto_relation applyto_phase modules) }
-sub mvp_aliases{  return { 'module' => 'modules' }  }
+sub mvp_aliases { return { 'module' => 'modules' } }
+
 sub current_version_of {
-    my ($self, $package ) = @_;
-    if ( $package eq 'perl' ){
-        # Thats not going to work, Dave.
-        return $];
-    }
-    require Module::Data;
-    my $data = Module::Data->new($package);
-    return if not $data;
-    return if not -e -f $data->path;
-    return $data->version;
+  my ( $self, $package ) = @_;
+  if ( $package eq 'perl' ) {
+
+    # Thats not going to work, Dave.
+    return $];
+  }
+  require Module::Data;
+  my $data = Module::Data->new($package);
+  return if not $data;
+  return if not -e -f $data->path;
+  return $data->version;
 }
 around dump_config => sub {
-    my ( $orig, $self ) = @_;
-    my $config = $self->$orig;
-    my $this_config = {
-        applyto_phase => $self->applyto_phase,
-        applyto_relation => $self->applyto_relation,
-        applyto          => $self->applyto,
-        modules => $self->modules,
-    };
-    $config->{'' . __PACKAGE__ } = $this_config;
-    return $config;
+  my ( $orig, $self ) = @_;
+  my $config      = $self->$orig;
+  my $this_config = {
+    applyto_phase    => $self->applyto_phase,
+    applyto_relation => $self->applyto_relation,
+    applyto          => $self->applyto,
+    modules          => $self->modules,
+  };
+  $config->{ '' . __PACKAGE__ } = $this_config;
+  return $config;
 };
+
 sub register_prereqs {
-    my ( $self ) = @_;
-    my $zilla = $self->zilla;
-    my $prereqs = $zilla->prereqs;
-    my $guts = $prereqs->cpan_meta_prereqs->{prereqs} || {};
-    my $anted = $self->_modules_hash;
+  my ($self)  = @_;
+  my $zilla   = $self->zilla;
+  my $prereqs = $zilla->prereqs;
+  my $guts = $prereqs->cpan_meta_prereqs->{prereqs} || {};
+  my $anted = $self->_modules_hash;
 
-    for my $applyto ( @{ $self->_applyto_list } ) {
-        my ( $phase, $rel ) = @{$applyto};
-        next if not exists $guts->{$phase};
-        next if not exists $guts->{$phase}->{$rel};
-        my $reqs = $guts->{$phase}->{$rel}->as_string_hash;
-        for my $module ( keys %{ $reqs }) {
-            next if not exists $self->_modules_hash->{$module};
-            my $latest = $self->current_version_of( $module );
-            if ( not defined $latest ) {
-                warn "You asked for the installed version of $module, and it is a dependency but it is apparently not installed";
-                next;
-            }
+  for my $applyto ( @{ $self->_applyto_list } ) {
+    my ( $phase, $rel ) = @{$applyto};
+    next if not exists $guts->{$phase};
+    next if not exists $guts->{$phase}->{$rel};
+    my $reqs = $guts->{$phase}->{$rel}->as_string_hash;
+    for my $module ( keys %{$reqs} ) {
+      next if not exists $self->_modules_hash->{$module};
+      my $latest = $self->current_version_of($module);
+      if ( not defined $latest ) {
+        warn "You asked for the installed version of $module, and it is a dependency but it is apparently not installed";
+        next;
+      }
 
-            $zilla->register_prereqs(
-                { phase => $phase, type => $rel },
-                $module, $latest
-            );
-        }
+      $zilla->register_prereqs( { phase => $phase, type => $rel }, $module, $latest );
     }
+  }
 }
 __PACKAGE__->meta->make_immutable;
 no Moose;
@@ -156,6 +158,67 @@ version 0.1.0
 =head1 SYNOPSIS
 
 This is based on the code of L<< C<Dist::Zilla::Plugin::Author::KENTNL::Prereqs::Latest::Selective>|Dist::Zilla::Plugin::Author::KENTNL::Prereqs::Latest::Selective >>, but intended for a wider audience.
+
+    [Prereqs::MatchInstalled]
+    module = My::Module
+
+B<NOTE:> Dependencies will only be upgraded to match the I<Installed> version if they're found elsewhere in the dependency tree.
+
+This is designed so that it integrates with other automated version provisioning.
+
+If you're hard-coding module deps instead, you will want to place this module I<after> other modules that declare deps.
+
+For instance:
+
+    [Prereqs]
+    Foo = 0
+
+    [Prereqs::MatchInstalled]
+    module = Foo
+
+^^ C<Foo> will be upgraded to the version installed.
+
+By default, dependencies that match values of C<module> will be upgraded when they are found in:
+
+    phase: build, test, runtime, configure, develop
+    relation: depends, suggests, recommends
+
+To change this behaviour, specify one or more of the following parameters:
+
+    applyto_phase = build
+    applyto_phase = configure
+
+    applyto_relation = requires
+
+etc.
+
+For more complex demands, this also works:
+
+    applyto = build.requires
+    applyto = configure.recommends
+
+And that should hopefully be sufficient to cover any conceivable usecase.
+
+Also note, we don't do any sort of sanity checking on the module list you provide.
+
+For instance,
+
+    module = strict
+    module = warning
+
+Will both upgrade the strict and warnings deps on your module, regardless of how daft an idea that may be.
+
+=begin MetaPOD::JSON v1.1.0
+
+    {
+        "namespace":"Dist::Zilla::Plugin::Prereqs::MatchInstalled",
+        "interface":"class",
+        "inherits":"Moose::Object",
+        "does":["Dist::Zilla::Role::PrereqSource","Dist::Zilla::Role::Plugin","Dist::Zilla::Role::ConfigDumper"]
+    }
+
+
+=end MetaPOD::JSON
 
 =head1 AUTHOR
 
