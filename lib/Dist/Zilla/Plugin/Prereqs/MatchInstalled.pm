d8700b3209c11ab77016cf81bd46a3b36e0443e6
Kent Fredric <kentfredric@gmail.com>
2013-08-31 14:47:49 +1200

    Build results of c868144 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Prereqs::MatchInstalled::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Prereqs::MatchInstalled::VERSION = '0.1.1';
+  $Dist::Zilla::Plugin::Prereqs::MatchInstalled::VERSION = '0.1.2';
 }
 
 # ABSTRACT: Depend on versions of modules the same as you have installed
@@ -111,6 +111,7 @@ sub mvp_aliases { return { 'module' => 'modules' } }
 sub current_version_of {
   my ( $self, $package ) = @_;
   if ( $package eq 'perl' ) {
+
     # Thats not going to work, Dave.
     return $];
   }
@@ -175,7 +176,7 @@ Dist::Zilla::Plugin::Prereqs::MatchInstalled - Depend on versions of modules the
 
 =head1 VERSION
 
-version 0.1.1
+version 0.1.2
 
 =head1 SYNOPSIS
 
