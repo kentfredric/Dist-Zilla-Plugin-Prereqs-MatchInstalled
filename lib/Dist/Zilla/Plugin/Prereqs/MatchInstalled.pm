93a0f0656771f711256b91137ad5a9fc5deaf713
Kent Fredric <kentfredric@gmail.com>
2014-07-19 16:49:10 +1200

    v1.000001
     [00 Trivial]
     - Minor packaging cleanups.
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000000, see misc/*.deps* for details
     - configure: (recommends: ↑1)
     - develop: +3 ↓3 -3 (suggests: +2 -1)
     - test: +2 ↓1 (recommends: +1 ↑1)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::MatchInstalled;
 
-our $VERSION = '1.000001';
+our $VERSION = '1.000002';
 
 # ABSTRACT: Depend on versions of modules the same as you have installed
 
