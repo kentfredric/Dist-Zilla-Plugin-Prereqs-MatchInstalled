430ebe31b0cb7604da575c8f8502f567c82b1c9e
Kent Fredric <kentfredric@gmail.com>
2014-07-19 16:49:22 +1200

    Build results of 93a0f06 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::MatchInstalled;
 
-our $VERSION = '1.000001';
+our $VERSION = '1.000002';
 
 # ABSTRACT: Depend on versions of modules the same as you have installed
 
@@ -368,7 +368,7 @@ Dist::Zilla::Plugin::Prereqs::MatchInstalled - Depend on versions of modules the
 
 =head1 VERSION
 
-version 1.000001
+version 1.000002
 
 =head1 SYNOPSIS
 
