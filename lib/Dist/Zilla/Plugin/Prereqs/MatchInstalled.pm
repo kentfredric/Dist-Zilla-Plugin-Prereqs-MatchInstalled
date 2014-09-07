c9966bdab4fd58cec7cefd234442ac50159ed97a
Kent Fredric <kentfredric@gmail.com>
2014-09-07 20:55:51 +1200

    v1.001002
     [Bugfix]
     - Don't fail on tests if Test::Difference is not installed.
     - Closes #3
     - Thanks Sander Lepik for reporting.
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001001, see misc/*.deps* for details
     - develop: (suggests: ↑1)

--- lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/MatchInstalled.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::MatchInstalled;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 # ABSTRACT: Depend on versions of modules the same as you have installed
 
